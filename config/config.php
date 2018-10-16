67ee8dc6290649de7668ef00534c8e8cca96fcb5
Jorge Dacosta <jorgemsdc@mail.com>
2018-10-16 13:55:14 +0100

    app config file

---
+++ config/config.php
@@ -0,0 +1,23 @@
+<?php
+
+    /**
+     * Global application settings
+     */
+    define ('DEBUG', true);
+	define ('DEFAULT_CONTROLLER', 'Home');
+	define ('DEFAULT_LAYOUT', 'shared');
+	define ('SITE_TITLE', 'Hero App');
+    define ('PROOT', '/app/');
+    
+    // Database config
+    define ('DB_HOST', '127.0.0.1');
+	define ('DB_NAME', 'app-db-name');
+    // In production mode
+    define ('DB_USER', 'db-user');
+    define ('DB_PASSWORD', 'db-passwrd');
+
+    // Cookie config
+    define ('CURRENT_USER_SESSION_NAME', 'kwXeusqldkiIKjehsLQZJFJ');
+	define ('REMEMBER_ME_COOKIE_NAME', 'AJJ212buasAsjaAsuja29Js');
+    define ('REMEMBER_ME_COOKIE_EXPIRY', 604800);
+    
