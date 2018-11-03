34e963c3cac764f0558524e40c9a26e9dd5c02cc
Jorge Dacosta <jorgemsdc@mail.com>
2018-11-03 22:11:15 +0100

    hotlinks refractor

--- app/views/layouts/shared.php
+++ app/views/layouts/shared.php
@@ -5,9 +5,11 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title><?= $this->getTitle() ?></title>
-    <link rel="stylesheet" href="<?=PROOT?>assets/css/bulma.min.css">
-    <script defer src="<?=PROOT?>assets/js/all.js"></script>
     <link rel="shortcut icon" href="<?=PROOT?>assets/images/favicon.ico">
+
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bulma/0.7.2/css/bulma.min.css">
+    <script defer src="https://use.fontawesome.com/releases/v5.3.1/js/all.js"></script>
+    <script defer src="https://use.fontawesome.com/releases/v5.3.1/js/all.js"></script>
     <?= $this->content('head') ?>
 </head>
 
